43d9cde3843161a2d34dbb4ee02d7cf3f36c4ef2
Pranav <84005308+pranavsutar@users.noreply.github.com>
2022-08-20 23:59:10 +0530

    initial commit

---
+++ src/components/Forms/Employeejoining.js
@@ -0,0 +1,76 @@
+ import React from 'react'
+ import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { useForm } from "react-hook-form";
+
+const Employeejoining = () => {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+   return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+    <Box
+    component="form"
+    sx={{
+      '& .MuiTextField-root': { m: 1, width: '25ch' },
+    }}
+    noValidate
+    autoComplete="off"
+  >
+    <div>
+      <TextField
+        required
+        id="fname"
+        label="First Name"
+        defaultValue=""
+        {...register("fname", 
+        {
+            required:true,
+            pattern:{
+                minLength:3,
+                maxLength: 50,
+            }
+            
+        })}
+       
+      />
+
+      {/* {errors.fname && <p style={{color:"red"}}> </p>} */}
+      <TextField
+       
+        id="fname"
+        label="Middle Name"
+        defaultValue=""
+        {...register("mname", 
+        {
+         
+            minLength:3,
+            maxLength: 50,
+        })}
+       
+      />
+
+ 
+      <TextField
+        
+        id="fname"
+        label="Last Name"
+        defaultValue=""
+        {...register("lname", 
+        {
+            minLength:3,
+            maxLength: 50,
+        })}
+       
+      />
+
+      
+    </div>
+  </Box>
+
+  <button type = "submit"> submit</button>
+  </form>
+   )
+ }
+ 
+ export default Employeejoining
---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Employeejoining from './Forms/Employeejoining'
+
+const Homepage = () => {
+  return (
+    <div>
+        <div className="container my-5">
+
+        <Employeejoining></Employeejoining>
+        </div>
+    </div>
+  )
+}
+
+export default Homepage
